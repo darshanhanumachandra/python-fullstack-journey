b98489d75980cc3a316496bcea243b1f6adae45f
Darshan H <darshanhanumachandra18@gmail.com>
2026-05-17 10:41:08 +0530

    Lesson 1: Understanding line-by-line execution

---
+++ lesson_01_basics.py
@@ -0,0 +1,5 @@
+print("Line 1 is running")
+print("Line 2 is running")
+print("Line 3 is running")
+primt("This line has a typo, it will cause an error")
+print("Line 5, will this run?")#this line will never run
